9f1ae8ed2a96bf7d532ffad12713e95056e14263
tsnash <tsnash@users.noreply.github.com>
2025-04-15 13:11:30 -0400

    modified user profile constants for clarity

--- src/main/java/net/spotapps/tester/UserProfileConstants.java
+++ src/main/java/net/spotapps/tester/UserProfileConstants.java
@@ -2,6 +2,7 @@ package net.spotapps.tester;
 
 public class UserProfileConstants {
     //Exception messages
-    public static final String INVALID_ID_MESSAGE = "Invalid id input [%s]";
-    public static final String USER_PROFILE_NOT_FOUND_EXCEPTION = "A user profile with id [%s] was not found.";
+    public static final String INVALID_ID_MESSAGE = "Invalid ID input [%s]";
+    public static final String USER_PROFILE_NOT_FOUND_MESSAGE = 
+        "A user profile with ID [%s] was not found.";
 }
