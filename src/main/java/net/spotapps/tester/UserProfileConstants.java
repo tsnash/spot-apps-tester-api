bd66d5f23388409e269e5b1df5b482b88f16f5fb
tsnash <tsnash@users.noreply.github.com>
2025-12-14 20:19:40 -0500

    distinguished invalid id messages

--- src/main/java/net/spotapps/tester/UserProfileConstants.java
+++ src/main/java/net/spotapps/tester/UserProfileConstants.java
@@ -3,7 +3,7 @@ package net.spotapps.tester;
 public class UserProfileConstants {
     // Exception messages
     public static final String INVALID_ID_MESSAGE = "Invalid ID input [%s]";
-    public static final String INVALID_ID_COLLECTION_MESSAGE = "Invalid ID input [%s]";
+    public static final String INVALID_ID_COLLECTION_MESSAGE = "Invalid ID inputs [%s]";
     public static final String USER_PROFILE_NOT_FOUND_MESSAGE = "A user profile with ID [%s] was not found.";
     public static final String USER_PROFILE_COLLECTION_NOT_FOUND_MESSAGE = "No user profiles with IDs [%s] were found.";
 }
